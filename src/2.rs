b2925ce7bf28a36cfe12cf0dc43ae874769c491a
sickmacisaac758 <sickmacisaac758@gmail.com>
2025-03-07 22:48:22 +0000

    Add 2.rs

---
+++ src/2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 4;
+    let y = 5;
+    if x + y == 9 {
+        println!("The sum of x and y is equal to 9");
+    } else {
+        println!("The sum of x and y is not equal to 9");
+    }
+}
