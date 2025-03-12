d782917d711f5bc6ca936d3c0680234eb3684085
sickmacisaac758 <sickmacisaac758@gmail.com>
2025-03-12 09:20:15 +0000

    Add 7.rs

---
+++ src/7.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x < 10 {
+        println!("x is less than 10");
+    } else if x > 10 {
+        println!("x is greater than 10");
+    } else {
+        println!("x is equal to 10");
+    }
+}
